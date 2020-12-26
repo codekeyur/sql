088042ca140bc90dde7557bf16a2f30e78ec6716
Keyur Patel <keyurpatel2868@gmail.com>
2020-12-26 10:55:10 -0500

    Databases have been created.

---
+++ student.sql
@@ -0,0 +1,15 @@
+
+CREATE DATABASE IF NOT EXISTS `student`;
+USE `student`;
+
+
+CREATE TABLE `names`(
+  `firstname` varchar(20) NOT NULL,
+  `lastname` varchar(20) NOT NULL
+  );
+INSERT INTO `names` VALUES ('Shilpa','Trail');
+INSERT INTO `names` VALUES ('Keyur','Patel');
+INSERT INTO `names` VALUES ('Ravi','Sakaria');
+INSERT INTO `names` VALUES ('Stone','John');
+INSERT INTO `names` VALUES ('Richard','Bailey');
+INSERT INTO `names` VALUES ('Reza','Lellahi');
