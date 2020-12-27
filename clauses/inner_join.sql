d744136608b7a05b18f1bdc5ccfb9223dcee3d63
Keyur Patel <keyurpatel2868@gmail.com>
2020-12-27 08:43:00 -0500

    clauses dir moved to SQL Hands-On.

--- clauses/inner_join.sql
+++
@@ -1,6 +0,0 @@
-USE sql_invoicing;
-SELECT payment_id, c.name AS client_name, c.state,amount,date,pm.name AS payment_type FROM payments p
-JOIN clients c
-     ON p.client_id = c.client_id
-JOIN payment_methods pm
-     ON p.payment_method = pm.payment_method_id
--- clauses/rollup.sql
+++
@@ -1,9 +0,0 @@
-USE sql_invoicing;
-SELECT
-      pm.name,
-      SUM(p.amount)
-FROM payments p
-JOIN  payment_methods pm
-ON p.payment_method = pm.payment_method_id
-GROUP BY pm.name WITH ROLLUP
-
