eea439712c1ce232945c97d8fc30c2f78723f758
Keyur Patel <keyurpatel2868@gmail.com>
2020-12-27 08:43:00 -0500

    clauses dir moved to SQL Hands-On.

--- clauses/group_by.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-	   date,
-       pm.name AS payment_method,
-       SUM(amount) AS total_payment
-FROM payments p
-JOIN payment_methods pm
-ON p.payment_method = pm.payment_method_id
-GROUP BY date,payment_method
-ORDER BY date
