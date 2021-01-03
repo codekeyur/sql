1a1b5c8624f3533bf3436683c0e50ea4c8571918
codekeyur <keyurpatel2868@gmail.com>
2021-01-03 14:01:32 -0500

    Using Trigger Delete Data from Table.

---
+++ SQL Hands-On/Trigger & Events /delete_trigger.sql
@@ -0,0 +1,10 @@
+DELIMITER $$
+CREATE TRIGGER payment_after_delete
+   AFTER DELETE ON payments
+   FOR EACH ROW
+BEGIN 
+  UPDATE invoices
+  SET payment_total = payment_total + OLD.amount
+  WHERE invoice_id = OLD.invoice_id;
+END $$
+DELIMITER ;
