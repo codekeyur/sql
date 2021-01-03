1bfb668fe431853420beecf29b4cc10b545c0f5b
codekeyur <keyurpatel2868@gmail.com>
2021-01-03 14:03:32 -0500

    Insert/Add data to table using Trigger.

---
+++ SQL Hands-On/Trigger & Events /insert_trigger.sql
@@ -0,0 +1,13 @@
+DELIMITER $$ 
+
+CREATE TRIGGER payments_after_insert
+  AFTER INSERT ON payments
+  FOR EACH ROW
+BEGIN 
+  UPDATE invoices
+  SET payment_total = payment_total + NEW.amount
+  WHERE invoice_id = NEW.invoice_id;
+END $$
+
+DELIMITER ;
+  
